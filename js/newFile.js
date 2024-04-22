1c06b5f721060fa1074c710407ac8869be6e31dd
Noelia Cuellar Chung <166728764+noeliacuellar3@users.noreply.github.com>
2024-04-22 09:26:45 -0400

    Delete js/newFile.js

--- js/newFile.js
+++
@@ -1,6 +0,0 @@
-for (let i = 0; i < 10; i++) {
-    estudiantes.push({
-        nombre: ["noelia", "Juan", "Matías", "Susan", "Carlos", "Pablo", "Roberto", "Ignacio", "Paulo", "Jaime"],
-        nota: generarNumeroAleatorio(20, 100)
-    });
-}
